267083d900c203ff35c3fec6ab61dce8b66def8b
Ritchie Ng <ritchieng@u.nus.edu>
2016-06-13 13:06:56 +0800

    Completed finding smallest and largest number scripts

--- README.md
+++ README.md
@@ -4,11 +4,11 @@
 Feel free to use these scripts in your projects. 
 
 ## Python Versions
-I have created two versions for each scripts to run on Python 2 and Python 3. The file names would have the suffixes _p2 and _p3 respectively.
+I have created two versions for each scripts to run on Python 2 and Python 3. The file names for Python 2 and Python 3 would have the suffixes _p2 and _p3 respectively. If the file runs on both Python 2 and Python 3, there will be no suffix _p2 and _p3.
 
 ### Scripts Available
 1. Normal Hours and Overtime Salary Computation (compute_pay_overtime_gross_p2.py & compute_pay_overtime_gross_p3.py)
-2. Seeking Smallest and Largest Number
+2. Seeking Smallest and Largest Number (find_smallest_largest_num_p2.py and find_smallest_largest_num_p3.py)
 3. Total, Average and Count Computation
 4. Collate list of unique words from a text file
 5. Calculate the most common word from a text file
---
+++ find_smallest_largest_num_p2.py
@@ -0,0 +1,43 @@
+# Find the smallest and largest numbers
+# This allows the user to enter a list of numbers until the user types done or press enter then the prompt would stop
+# Author: Ritchie Ng
+
+largest = None
+smallest = None
+
+while True:
+    num = raw_input('Enter a number: ')
+
+    # Handle edge cases
+    if num == 'done':
+        break
+    # Allows user to press enter to complete
+    if len(num) < 1:
+        break
+
+    # Try and Except to catch input errors
+    try:
+        num = float(num)
+    except:
+        print 'Invalid Input'
+        # Jumps to the start of the loop without running the code below
+        continue
+
+    # This will be permanently false after the first iteration
+    if smallest is None:
+        smallest = num
+    # Replaces the iteration variable with smaller input num
+    if num < smallest:
+        smallest = num
+
+    # This will be permanently false after the first iteration
+    if largest is None:
+        largest = num
+    # Replaces the iteration variable with larger input num
+    elif num > largest:
+        largest = num
+
+print "Maximum number:", largest
+print "Smallest number:", smallest
+
+
