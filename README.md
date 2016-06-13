101f5292bde2eba2ff7f7b43c30efedaf81cc99a
Ritchie Ng <ritchieng@u.nus.edu>
2016-06-13 12:54:38 +0800

    Completed python scripts for gross pay

--- README.md
+++ README.md
@@ -1,10 +1,13 @@
 # Useful Python Scripts
 
 ## Details on Python Scripts
-These scripts can be used in your projects.
+Feel free to use these scripts in your projects. 
+
+## Python Versions
+I have created two versions for each scripts to run on Python 2 and Python 3. The file names would have the suffixes _p2 and _p3 respectively.
 
 ### Scripts Available
-1. Normal Hours and Overtime Salary Computation
+1. Normal Hours and Overtime Salary Computation (compute_pay_overtime_gross_p2.py & compute_pay_overtime_gross_p3.py)
 2. Seeking Smallest and Largest Number
 3. Total, Average and Count Computation
 4. Collate list of unique words from a text file
---
+++ compute_pay_overtime_gross_p3.py
@@ -0,0 +1,36 @@
+# Python 2+
+# Compute gross pay with overtime parameters
+# Author: Ritchie Ng
+
+# Get hourly rate
+hourly_rate = input('Hourly rate: ')
+# Convert hourly rate to float
+hourly_rate = float(hourly_rate)
+
+# Get number of hours
+hours = input('Number of hours: ')
+# Convert number of hours to float
+hours = float(hours)
+
+# Get overtime multiplier
+# overtime_m = 1.5 means there is a 50% premium for overtime hours over normal hours
+overtime_m = input('Overtime pay multiplier (number of times of normal pay rate): ')
+overtime_m = float(overtime_m)
+
+# Get number of hours until overtime takes effect
+overtime_ot = input('Number of hours at which overtime takes effect: ')
+overtime_ot = float(overtime_ot)
+
+# Creating function to compute gross pay from regular and overtime hours
+def computepay(h, r, m, ot):
+    # Overtime rate
+    overtime_r = r * m
+
+    if h < ot:
+        return h * r
+    else:
+        return (ot * r) + (h - ot) * overtime_r
+
+# Key in parameters to the computepay() function
+pay = computepay(hours, hourly_rate, overtime_m, overtime_ot)
+print(pay)
